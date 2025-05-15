1e2c31a7dfb0843e5cba87d999521cf926f7fac8
Suraj Sedai <surajsedai05@gmail.com>
2025-05-15 18:38:14 -0400

    models.py

--- backend/nubuzz/models.py
+++ backend/nubuzz/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.TextField()
+    content = models.TextField()
+    url = models.URLField()
+    category = models.CharField(max_length=100)
+    published_at = models.DateTimeField()
+    sentiment = models.CharField(max_length=20)
+    location = models.CharField(max_length=100)
