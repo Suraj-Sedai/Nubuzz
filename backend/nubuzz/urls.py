29a61322b62634b931c7904124103c5f4eb0178f
Suraj Sedai <surajsedai05@gmail.com>
2025-05-15 18:47:13 -0400

    inital view

---
+++ backend/nubuzz/urls.py
@@ -0,0 +1,8 @@
+from .views import fetch_news
+from django.urls import path
+from django.http import JsonResponse
+
+app_name = 'nubuzz'
+urlpatterns = [
+    path('fetch-news/', lambda request: JsonResponse(fetch_news())),
+]
--- backend/nubuzz/views.py
+++ backend/nubuzz/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Article
 # Create your views here.
+import requests
+def fetch_news():
+    api_key = '15866422dea04a8b85fc0ae163cdca13'
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
+    data = requests.get(url).json()
+    for article in data['articles']:
+        Article.objects.create(
+            title=article['title'],
+            content=article['content'],
+            url=article['url'],
+            published_at=article['publishedAt'],
+            category=article['category'],
+            sentiment=article['sentiment'],
+            location=article['location']
+        )
