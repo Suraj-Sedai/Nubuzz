4afcec8d89e5f495b6af1332f508129d915d4ebe
Suraj Sedai <surajsedai05@gmail.com>
2025-05-15 18:51:51 -0400

    ok, news fetched

--- backend/nubuzz/views.py
+++ backend/nubuzz/views.py
@@ -3,10 +3,15 @@ from django.http import JsonResponse
 from .models import Article
 # Create your views here.
 import requests
+
 def fetch_news():
     api_key = '15866422dea04a8b85fc0ae163cdca13'
     url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
-    data = requests.get(url).json()
+    response = requests.get(url)
+    data = response.json()
+    if data['status'] != 'ok':
+        return JsonResponse({'error': 'Failed to fetch news'})
+    
     for article in data['articles']:
         Article.objects.create(
             title=article['title'],
@@ -17,3 +22,5 @@ def fetch_news():
             sentiment=article['sentiment'],
             location=article['location']
         )
+        
+    return data
