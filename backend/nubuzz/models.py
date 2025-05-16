1efed6e3c192936ea7cf3058c2d6ea2f4a3eb64d
Suraj Sedai <surajsedai05@gmail.com>
2025-05-15 22:57:54 -0400

    model, category

--- backend/nubuzz/models.py
+++ backend/nubuzz/models.py
@@ -5,6 +5,7 @@ class Article(models.Model):
     source_id         = models.CharField(max_length=100, blank=True)
     source_name       = models.CharField(max_length=100, blank=True)
     author            = models.CharField(max_length=200, blank=True)
+    category          = models.CharField(max_length=100, blank=True)
     title             = models.CharField(max_length=300)
     description       = models.TextField(blank=True)
     content           = models.TextField(blank=True)
